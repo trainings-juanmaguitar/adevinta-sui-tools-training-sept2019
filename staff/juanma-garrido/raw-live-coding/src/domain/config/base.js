83808b5d1d8671d5d86d6b19f796a78f15aa259d
JuanMa Garrido <juanma.garrido@gmail.com>
2019-09-25 09:20:49 +0200

    docs(META): demos

---
+++ staff/juanma-garrido/raw-live-coding/src/domain/config/base.js
@@ -0,0 +1,3 @@
+export default {
+  THEMOVIEDB_API_BASE_URL: 'api.themoviedb.org/3'
+}
---
+++ staff/juanma-garrido/raw-live-coding/src/domain/config/index.js
@@ -0,0 +1,45 @@
+import base from './base'
+
+// import test from './environments/test'
+// import development from './environments/development'
+// import production from './environments/production'
+// import preproduction from './environments/preproduction'
+
+const envConfig = {
+  //   development,
+  //   preproduction,
+  //   production,
+  //   test
+}
+
+const environment = process.env.NODE_ENV
+
+export default class Config {
+  constructor(propsConfig) {
+    this._config = {
+      ...propsConfig,
+      ...base,
+      ...envConfig[environment]
+    }
+  }
+
+  /**
+   * @method
+   * @param {String} key
+   * @return {*}
+   */
+  get(key) {
+    return this._config[key]
+  }
+
+  /**
+   * @method
+   * @param {String} key
+   * @param {*} value
+   * @return {*}
+   */
+  set(key, value) {
+    this._config[key] = value
+    return this
+  }
+}
